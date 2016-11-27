c0a3cda0a6a0e0ebe503699f20241b60dda9fbf9
Jakubinyi <jakubinyi@gmail.com>
2016-11-28 00:26:00 +0100

    printing refactoring

--- printing.py
+++ printing.py
@@ -1,8 +1,28 @@
 from reports import *
 # Printing functions
 
-print(count_games("game_stat.txt"))
-print(decide("game_stat.txt", 1998))
-print(get_latest("game_stat.txt"))
-print(count_by_genre("game_stat.txt", "RPG"))
-print(get_line_number_by_title("game_stat.txt", "Diablo II"))
+
+def printer_function(function_name, parameter=None, file_name="game_stat.txt"):
+    """ The printer_function  print the return value(s) of the report function.
+
+    Arguments:
+    function_name -- the name of the imported function
+    parameter -- the parameter of the imported function (default: None)
+    file_name -- the name of the input file of the imported function (default: game_stat.txt)
+    """
+    if parameter == None:
+        print(function_name(file_name))
+    else:
+        print(function_name(file_name, parameter))
+
+
+def main():
+    printer_function(count_games)
+    printer_function(decide, 1998)
+    printer_function(get_latest)
+    printer_function(decide, 1998)
+    printer_function(count_by_genre, "RPG")
+
+
+if __name__ == '__main__':
+    main()
